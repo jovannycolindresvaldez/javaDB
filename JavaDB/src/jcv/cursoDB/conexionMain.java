9fd6c742117c6d8957c83997be46d4fb98e956f2
Jovanny Colindres <Jovanny Colindres@jovacova>
2016-08-29 23:11:51 -0500

    Insertat a la DB con parametros

--- JavaDB/src/jcv/cursoDB/conexionMain.java
+++ JavaDB/src/jcv/cursoDB/conexionMain.java
@@ -12,6 +12,14 @@ import java.sql.SQLException;
 public class conexionMain {
 
     public static void main(String[] args) throws SQLException {
+        conexionMain obj= new conexionMain();
+        String nombre="paola", email="paola@gmail.com",pwd="54321";
+        obj.registrarDB(nombre,email,pwd);
+        
+    }
+    
+    public void registrarDB(String nombre, String email, String pwd) throws SQLException
+    {
         //JDBC driver nombre y base de datos URL
         final String JDBC_DRIVER= "com.mysql.jdbc.Driver";
         final String DB_URL="jdbc:mysql://localhost/01curso_php";
@@ -23,8 +31,15 @@ public class conexionMain {
         try {
             Class.forName(JDBC_DRIVER);
             conexion= DriverManager.getConnection(DB_URL, USER, PASS);
-            String query= "INSERT INTO user (nombre, email, password) VALUES('mit', 'jcv@hotmail.com', '123456')";
+            //String query= "INSERT INTO user (nombre, email, password) VALUES('mit', 'jcv@hotmail.com', '123456')";
+            
+            String query= "INSERT INTO user (nombre, email, password) VALUES(?,?,?)";
+            
             PreparedStatement st= conexion.prepareStatement(query);
+            st.setString(1, nombre);
+            st.setString(2, email);
+            st.setString(3, pwd);
+            
             st.executeUpdate();
             st.close();
         } catch (Exception e) {
